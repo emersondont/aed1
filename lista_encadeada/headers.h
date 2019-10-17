bd5e462ff5dc7e11e5cc23af18beb1916b320b1e
Emerson Vieira <emerson.vv75248@gmail.com>
2019-10-17 12:34:02 -0300

    .

---
+++ lista_encadeada/headers.h
@@ -0,0 +1,22 @@
+﻿#ifndef HEADERS_H
+#define HEADERS_H
+
+typedef struct info{
+	int cod;
+	char nome[20];
+	double valor;
+}SInfo;
+
+typedef struct nodo{
+	SInfo dado;
+	struct nodo *pPrevius;
+	struct nodo *pNext;
+}SNodo;
+
+typedef struct lista{
+	unsigned int nCount;
+	SNodo *pLast;
+	SNodo *pFirst;
+}SLista;
+
+#endif
